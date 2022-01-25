118e6c27ebbd75ee7da2180a130449cabfd0c68e
Cristian Arriagada <cristian.arriagada.jara@gmail.com>
2022-01-25 19:44:19 -0300

    comit inicial de gradle

---
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.devopsusach2020;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsUsach2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+		String correcto = "hola";
+		Assertions.assertThat(correcto).isEqualTo("hola");
+	}
+
+}
