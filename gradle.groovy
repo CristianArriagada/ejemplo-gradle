f5eefb15cf99bafdfc182c45744885c62259144d
Cristian Arriagada <cristian.arriagada.jara@gmail.com>
2022-02-01 16:43:59 -0300

    modificacion archivos groovy

--- gradle.groovy
+++ gradle.groovy
@@ -3,21 +3,25 @@
 	def ejecucion = load 'script.groovy'
 	ejecucion.call()
 */
-def call(){
-    stage("Paso 1: Build && Test"){
+def call(stages){
+    env.STAGE = "Paso 1: Build  Test"
+    stage("$env.STAGE"){
         sh "gradle clean build"
     }
-    stage("Paso 2: Sonar - Análisis Estático"){
+    env.STAGE = "Paso 2: Sonar - Análisis Estático"
+    stage("$env.STAGE"){
         sh "echo 'Análisis Estático!'"
         withSonarQubeEnv('sonarqube') {
             sh 'chmod +x gradlew && ./gradlew sonarqube -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
         }
     }
-    stage("Paso 3: Curl Springboot Gradle sleep 20"){
+    env.STAGE = "Paso 3: Curl Springboot Gradle sleep 20"
+    stage("$env.STAGE"){
         sh "gradle bootRun&"
         sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
-    stage("Paso 4: Subir Nexus"){
+    env.STAGE = "Paso 4: Subir Nexus"
+    stage("$env.STAGE"){
         nexusPublisher nexusInstanceId: 'nexus',
         nexusRepositoryId: 'devops-usach-nexus',
         packages: [
@@ -37,13 +41,16 @@ def call(){
             ]
         ]
     }
-    stage("Paso 5: Descargar Nexus"){
+    env.STAGE = "Paso 5: Descargar Nexus"
+    stage("$env.STAGE"){
         sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
     }
-    stage("Paso 6: Levantar Artefacto Jar"){
+    env.STAGE = "Paso 6: Levantar Artefacto Jar"
+    stage("$env.STAGE"){
         sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
     }
-    stage("Paso 7: Testear Artefacto - Dormir(Esperar 20sg) "){
+    env.STAGE = "Paso 7: Testear Artefacto - Dormir(Esperar 20sg)"
+    stage("$env.STAGE"){
         sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
 }
